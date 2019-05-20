a346e4b76d7fe8c27bf2f2836c28b3f984a673d5
junothyme <junothyme@gmail.com>
2019-05-20 10:32:23 +0900

    Initial Commit - Lab 3

---
+++ Lab3/TimeSheet.cpp
@@ -0,0 +1,37 @@
+#include "Timesheet.h"
+
+namespace lab3
+{
+	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
+	{
+	}
+
+	void TimeSheet::AddTime(int timeInHours)
+	{
+	}
+
+	int TimeSheet::GetTimeEntry(unsigned int index) const
+	{
+		return 0;
+	}
+
+	int TimeSheet::GetTotalTime() const
+	{
+		return 0;
+	}
+
+	float TimeSheet::GetAverageTime() const
+	{
+		return 0.0f;
+	}
+
+	float TimeSheet::GetStandardDeviation() const
+	{
+		return 0.0f;
+	}
+
+	const std::string& TimeSheet::GetName() const
+	{
+		return "temporary";
+	}
+}
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+
+#include "TimeSheet.h"
+
 int main()
 {
+	lab3::TimeSheet employee1("John", 10);
+	employee1.AddTime(4);
+	employee1.AddTime(7);
+	std::cout << "Employee: " << employee1.GetName() << " / AverageTime: " << employee1.GetAverageTime() << " / TotalTime: " << employee1.GetTotalTime() << std::endl;
 
+	return 0;
 }

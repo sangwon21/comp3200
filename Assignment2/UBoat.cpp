3f038d230369aac91c5f6d13f9865dd40aadb76c
junothyme <junothyme@gmail.com>
2019-05-27 15:58:48 +0900

    Implementing UBoat Class - Assignment 2

--- Assignment2/UBoat.cpp
+++ Assignment2/UBoat.cpp
@@ -1,12 +1,36 @@
 #include "UBoat.h"
+#include <cmath>
 
 namespace assignment2
 {
 	UBoat::UBoat()
+		: Vehicle(50)
 	{
 	}
 
 	UBoat::~UBoat()
 	{
+	}
+
+	unsigned int UBoat::GetMaxSpeed()
+	{
+		const unsigned int compareSpeedLhs = GetDiveSpeed();
+		const unsigned int compareSpeedRhs = GetSailSpeed();
+		const unsigned int maxSpeed = compareSpeedLhs > compareSpeedRhs ? compareSpeedLhs : compareSpeedRhs;
+		return maxSpeed;
+	}
+
+	unsigned int UBoat::GetDiveSpeed()
+	{
+		const unsigned int sumOfWeight = this->GetSumOfWeight();
+		return 500 * log((sumOfWeight + 150) / 150) + 30;
+	}
+
+	unsigned int UBoat::GetSailSpeed()
+	{
+		const unsigned int compareSpeedLhs = 200;
+		const unsigned int compareSpeedRhs = 550 - this->GetSumOfWeight() / 10;
+		const unsigned int sailSpeed = compareSpeedLhs > compareSpeedRhs ? compareSpeedLhs : compareSpeedRhs;
+		return sailSpeed;
 	}
 }
--- Assignment2/UBoat.h
+++ Assignment2/UBoat.h
@@ -1,11 +1,19 @@
 #pragma once
 
+#include "Vehicle.h"
+#include "IDivable.h"
+#include "ISailable.h"
+
 namespace assignment2
 {
-	class UBoat
+	class UBoat : public Vehicle, public IDivable, public ISailable
 	{
 	public:
 		UBoat();
 		~UBoat();
+
+		unsigned int GetMaxSpeed();
+		unsigned int GetDiveSpeed();
+		unsigned int GetSailSpeed();
 	};
 }

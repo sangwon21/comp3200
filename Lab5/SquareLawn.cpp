6835959e5c315cb3f812c83fde098443d8349c20
junothyme <junothyme@gmail.com>
2019-06-03 21:52:16 +0900

    Implementing SquareLawn Class - Lab 5

--- Lab5/SquareLawn.cpp
+++ Lab5/SquareLawn.cpp
@@ -1,6 +1,7 @@
 #include "SquareLawn.h"
 
 lab5::SquareLawn::SquareLawn(double width)
+	: Lawn(width)
 {
 }
 
@@ -10,15 +11,68 @@ lab5::SquareLawn::~SquareLawn()
 
 unsigned int lab5::SquareLawn::GetArea() const
 {
-	return 0;
+	double area = mWidth * mHeight;
+
+	unsigned int intValue = static_cast<unsigned int>(area);
+
+	if (static_cast<double>(intValue) == area)
+	{
+		return intValue;
+	}
+	return intValue + 1;
 }
 
 unsigned int lab5::SquareLawn::GetMinimumFencesCount() const
 {
-	return 0;
+
+	double widthFence = mWidth / 0.25;
+	double heightFence = mHeight / 0.25;
+
+	unsigned int widthCount = static_cast<unsigned int>(widthFence);
+
+	if (static_cast<double>(widthCount) != widthFence)
+	{
+		widthCount++;
+	}
+
+	unsigned int heightCount = static_cast<unsigned int>(heightFence);
+
+	if (static_cast<double>(heightCount) != heightFence)
+	{
+		heightCount++;
+	}
+
+	return widthCount * 2 + heightCount * 2;
 }
 
 unsigned int lab5::SquareLawn::GetFencePrice(eFenceType fenceType) const
 {
-	return 0;
+	unsigned int widthDist = static_cast<unsigned int>(mWidth);
+
+	if (static_cast<double>(widthDist) != mWidth)
+	{
+		widthDist++;
+	}
+
+	unsigned int heightDist = static_cast<unsigned int>(mHeight);
+
+	if (static_cast<double>(heightDist) != mHeight)
+	{
+		heightDist++;
+	}
+
+	unsigned int circumference = widthDist * 2 + heightDist * 2;
+	unsigned int price = 0;
+
+	switch (fenceType)
+	{
+	case eFenceType::RED_CEDAR:
+		price = 6 * circumference;
+		break;
+	case eFenceType::SPRUCE:
+		price = 7 * circumference;
+		break;
+	}
+
+	return price;
 }

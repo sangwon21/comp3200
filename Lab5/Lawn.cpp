5572f805e9e327da07f58c56f0fb0f2b82b2b73b
junothyme <junothyme@gmail.com>
2019-06-03 20:56:10 +0900

    Implementing GetGrassPrice() - Lab 5

--- Lab5/Lawn.cpp
+++ Lab5/Lawn.cpp
@@ -1,8 +1,21 @@
 #include "Lawn.h"
+#include "eGrassType.h"
 
 namespace lab5
 {
 	Lawn::Lawn()
+	{
+	}
+
+	Lawn::Lawn(double width)
+		: mWidth(width),
+		  mHeight(width)
+	{
+	}
+
+	Lawn::Lawn(double width, double height)
+		: mWidth(width),
+		  mHeight(height)
 	{
 	}
 
@@ -12,7 +25,32 @@ namespace lab5
 
 	unsigned int Lawn::GetGrassPrice(eGrassType grassType) const
 	{
-		return 0;
+		unsigned int area = this->GetArea();
+
+		double price = 0.0;
+		switch (grassType)
+		{
+		case eGrassType::BAHIA:
+			price *= 5;
+			break;
+		case eGrassType::BERMUDA:
+			price *= 8;
+			break;
+		case eGrassType::BENTGRASS:
+			price *= 3;
+			break;
+		case eGrassType::PERENNIAL_RYEGRASS:
+			price *= 2.5;
+			break;
+		case eGrassType::ST_AUGUSTINE:
+			price *= 4.5;
+			break;
+		}
+
+		price = price * 10 + 0.5;
+		unsigned int intValue = static_cast<unsigned int>(price);
+
+		return intValue;
 	}
 
 	unsigned int Lawn::GetMinimumSodRollsCount() const

b4f24b8f1b20692dfa01c20237d00d843af7d172
junothyme <junothyme@gmail.com>
2019-06-10 12:07:18 +0900

    Setting the Basics for the Project - Lab 6

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,35 @@
+#include "Lab6.h"
+
+namespace lab6
+{
+	int Sum(const std::vector<int>& v)
+	{
+		return 0;
+	}
+
+	int Min(const std::vector<int>& v)
+	{
+		return 0;
+	}
+
+	int Max(const std::vector<int>& v)
+	{
+		return 0;
+	}
+
+	float Average(const std::vector<int>& v)
+	{
+		return 0.0f;
+	}
+
+	int NumberWithMaxOccurrence(const std::vector<int>& v)
+	{
+		return 0;
+	}
+
+	void SortDescending(std::vector<int>& v)
+	{
+
+	}
+
+}

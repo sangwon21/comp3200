31c503cee568db548ffbdf6de8dfb4d68326043b
junothyme <junothyme@gmail.com>
2019-07-15 16:50:00 +0900

    Setting the Basics for the Project and Implementing Constructors for TreeNode - Assignment 4

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -1,4 +1,19 @@
+#include <cassert>
+#include "TreeNode.h"
+#include "BinarySearchTree.h"
+#include <iostream>
+
+using namespace assignment4;
+using namespace std;
+
 int main()
 {
+	TreeNode<int> n(std::make_unique<int>(2));
 
+	std::shared_ptr<TreeNode<int>> parent = std::make_shared<TreeNode<int>>(std::make_unique<int>(5));
+	TreeNode<int> n2(parent, std::make_unique<int>(2));
+
+	cout << "The End of the Test" << endl;
+	cin.get();
+	return 0;
 }
